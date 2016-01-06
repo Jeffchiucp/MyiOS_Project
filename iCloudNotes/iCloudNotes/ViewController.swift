431b9a85b9b35a610aa9a22b6877f0afedc3d0d6
jeff <jchiu@cloudpassage.com>
2016-01-06 13:42:24 -0800

    iCloudNotes

---
+++ iCloudNotes/iCloudNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iCloudNotes
+//
+//  Created by Jeff_Chiu on 1/6/16.
+//  Copyright © 2016 Jeff_Chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
